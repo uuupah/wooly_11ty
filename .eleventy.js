53dbc2bb0009b719d3e08e18e2abd8cf98289b5e
hollis <uuupah@gmail.com>
2024-10-26 17:44:48 +1030

    add coloured tags to homepage

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,23 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/img");
   // eleventyConfig.addPassthroughCopy("./src/fonts");
   // eleventyConfig.addPassthroughCopy("./src/js");
+  eleventyConfig.addShortcode("tags", function (tagList) {
+    var out = "";
+    for (tag in tagList) {
+      out += `<div class="tag-span">${tag}</div>`;
+      //   if (tag in grey_tags) {
+      //     out += `<div class="tag-span tag-grey">${tag}</div>`;
+      //   } else if (tag in red_tags) {
+      //     console.log("red");
+      //     out += `<div class="tag-span tag-red">${tag}</div>`;
+      //   } else {
+      //     console.log("other");
+      //     out += `<div class="tag-span">${tag}</div>`;
+      //   }
+    }
+    // return out;
+    return tagList;
+  });
 
   return {
     dir: {
